bafffbab9fbb8b2fb9c10497c82c2f0879bf08fe
LevenusSupremus <watermzfkmelon@gmail.com>
2021-06-16 18:33:42 +0300

    second commit?

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=1250)
 
     def __str__(self):
         return self.title
